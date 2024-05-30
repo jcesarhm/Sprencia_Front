b585e18ee43a5c7c82812ff71fd0a8cfe8d99b2d
jcesarhm <julio.cesar.he@icloud.com>
2024-05-30 07:19:24 +0200

    change of better

---
+++ src/app/interfaces/activity.ts
@@ -0,0 +1,16 @@
+export interface Activity {
+ id: number;
+ name: string;
+ summary: string;
+ price: number;
+ date: string;
+}
+
+ export interface ActivityDetail{
+    id: number;
+    name: string;
+    summary: string;
+    price: number;
+    date: string;
+    description: string;
+ }
